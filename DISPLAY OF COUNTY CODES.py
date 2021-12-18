812796a66e473d10bf90dbe4b177c22db5fa6236
Ezra Were <85166735+EZRA-WERE@users.noreply.github.com>
2021-12-18 08:06:05 +0300

    Add files via upload

---
+++ DISPLAY OF COUNTY CODES.py
@@ -0,0 +1,145 @@
+county_name=input("Enter the county_name starting with capital letter then lower class alphabets:")
+if county_name=="Bungoma":
+    print("Bungoma code is",+ 39)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name=="Mombasa":
+    print("Mombasa code is", + 1)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name=="Baringo":
+    print("Baringo code is",+ 30)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Uasin Gishu":
+    print("Uasin Gishu code is", + 27)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "West Pokot":
+    print("West Pokot code is", + 24)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Migori":
+    print("Migori code is", + 44)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Elgeiyo Marakwet":
+    print("Elgeiyo Marakwet code is", + 28)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Homabay":
+    print("Homabay code is", + 43)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Siaya":
+    print("Siaya code is", +41)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kiambu":
+    print("Kiambu code is", + 22)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Machakos":
+    print("Machakos code is", + 16)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Vihiga":
+    print("Vihiga code is", + 38)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nakuru":
+    print("Nakuru code is", + 32)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Makueni":
+    print("Makueni code is", + 17)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Narok":
+    print("Narok code is", + 33)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Tana River":
+    print("Tana River code is", +4)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Meru":
+    print("Meru code is", + 12)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Garissa":
+    print("Garissa code is", + 7)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kajiado":
+    print("Kajiado code is", + 34)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kilifi":
+    print("Kilifi code is", + 3)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Wajir":
+    print("Wajir code is",  8)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Laikipia":
+    print("Laikipia code is", + 31)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kitui":
+    print("Kitui code is", + 15)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Turkana":
+    print("Turkana code is", +23)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Lamu":
+    print("Lamu code is", +5)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kwale":
+    print("Kwale code is", + 2)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kericho":
+    print("Kericho code is", + 35)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kisumu":
+    print("Kisumu code is", +42)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Samburu":
+    print("Samburu code is", + 25)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Murang'a":
+    print("Murang'a code is", + 21)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Embu":
+    print("Embu code is", + 14)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Marsabit":
+    print("Marsabit code is", + 10)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kissii":
+    print("Kissii code is", + 45)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Trans Nzoia":
+    print("Trans Nzoia code is", + 26)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nyandarua":
+    print("Nyandarua code is", + 18)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Tharakanithi":
+    print("Tharakanithi code is", + 13)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Mandera":
+    print("Mandera code is", + 9)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Taita Taveta":
+    print("Taita Taveta code is", + 6)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Busia":
+    print("Busia code is", + 40)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Bomet":
+    print("Bomet code is", + 36)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kirinyaga":
+    print("Kirinyaga code is", + 20)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Kakamega":
+    print("Kakamega code is", + 37)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nyeri":
+    print("Nyeri code is", + 19)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nandi":
+    print("Nandi code is", + 29)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nyamira":
+    print("Nyamira code is", + 46)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Nairobi":
+    print("Nairobi code is", + 47)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+elif county_name == "Isiolo":
+    print("Isiolo code is", + 11)
+    print("WELOME TO KENYA! KENYA HAS 47 COUNTIES WITH THEIR RESPECTIVE CODES! HER LEADER IS HIS EXCELENCY UHURU KENYATTA.")
+else:
+    print("invalid input!")
+
